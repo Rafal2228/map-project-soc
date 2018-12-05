ee884778e4b1cdd3294935e42c2518e61af16d76
Rafal2228 <rafal2228@gmail.com>
2018-12-05 16:25:09 +0100

    feature(map-background): init coordinates generation

--- src/game/components/MapBackground.jsx
+++ src/game/components/MapBackground.jsx
@@ -1,26 +1,52 @@
 // @flow
 import React from 'react';
-import { MapTile } from './MapTile';
 import './MapBackground.scss';
 
-interface MapBackgroundProps {
-  coordinates: string[];
-  viewBox: string;
+function generateCoordinates(width: number, height: number, spaceBetween: number, maxControlPointSpace: number) {
+  const horizontalCurves = [];
+  const verticalCurves = [];
+
+  const horizontalLength = Math.floor(height / spaceBetween);
+  for (let i = 1; i <= horizontalLength; i++) {
+    const fromTop = i * spaceBetween;
+    const controlFromTop = fromTop + ((horizontalLength / 2 - i) / horizontalLength) * maxControlPointSpace;
+    horizontalCurves.push(
+      `M
+      0,${fromTop}
+      C
+      ${maxControlPointSpace},${controlFromTop}
+      ${width - maxControlPointSpace},${controlFromTop}
+      ${width},${fromTop}`
+    );
+  }
+
+  return [...horizontalCurves, ...verticalCurves];
 }
 
-export function MapBackground(props: MapProps) {
-  const { coordinates } = props;
-  const coordinateItems = coordinates && coordinates.map((d, index) => (
+interface MapBackgroundProps {
+  viewBox: string;
+  coordinates: string[];
+  islands: string[];
+}
+
+export function MapBackground(props: MapBackgroundProps) {
+  const { islands } = props;
+  const coordinateItems = generateCoordinates(100, 100, 10, 5).map((d, index) => (
     <path key={`coordinate-${index}`} className="map-background__coordinate" d={d} />
   ));
 
+  const islandItems =
+    islands &&
+    islands.map((d, index) => <path key={`coordinate-${index}`} className="map-background__coordinate" d={d} />);
+
   return (
     <div className="map-background">
-      <svg className="map-background__svg" viewBox={props.viewBox}>
+      <svg className="map-background__svg" viewBox="0 0 100 100">
+        <g className="map-background__coordinates-wrapper">{coordinateItems}</g>
 
-        <g className="map-background__coordinates-wrapper">
-          {coordinateItems}
-        </g>
+        <svg width="100%" height="100%" viewBox={props.viewBox}>
+          <g className="map-background__islands-wrapper">{islandItems}</g>
+        </svg>
       </svg>
     </div>
   );
--- src/game/containers/Main.jsx
+++ src/game/containers/Main.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Game from './Game';
 import Home from './Home';
+import { MapBackground } from '../components/MapBackground';
+import { MAP_VIEWBOX, MAP_BACKGROUND_ISLANDS, MAP_BACKGROUND_COORDINATES } from '../constants';
 
 function mapStateToProps(state) {
   const { started } = state && state.game;
@@ -18,12 +20,19 @@ interface MainProps {
 
 function Main(props: MainProps) {
   const { started } = props;
+  let content = null;
 
   if (started) {
-    return <Game />;
+    content = <Game />;
+  } else {
+    content = <Home />;
   }
 
-  return <Home />;
+  return (
+    <MapBackground viewBox={MAP_VIEWBOX} islands={MAP_BACKGROUND_ISLANDS} coordinates={MAP_BACKGROUND_COORDINATES}>
+      {content}
+    </MapBackground>
+  );
 }
 
 export default connect(mapStateToProps)(Main);
