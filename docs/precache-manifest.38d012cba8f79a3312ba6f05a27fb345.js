467fc6f42f2faa58fe9600f81f4705e62bb6b0ee
Rafal2228 <rafal2228@gmail.com>
2019-01-15 13:51:36 +0100

    chore(docs): update docs public_url

---
+++ docs/precache-manifest.38d012cba8f79a3312ba6f05a27fb345.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8e0a5d61d66c47dcea3",
+    "url": "/map-project/static/js/runtime~main.e8e0a5d6.js"
+  },
+  {
+    "revision": "010873b8d987bff4de00",
+    "url": "/map-project/static/js/main.010873b8.chunk.js"
+  },
+  {
+    "revision": "dde10b45b1b6d3a3159c",
+    "url": "/map-project/static/js/1.dde10b45.chunk.js"
+  },
+  {
+    "revision": "010873b8d987bff4de00",
+    "url": "/map-project/static/css/main.cce610b2.chunk.css"
+  },
+  {
+    "revision": "a8e6cc87aadd730210d5848b11f4d601",
+    "url": "/map-project/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.21179407415026b21a6b2b8f830a4a40.js"
+  "/map-project/precache-manifest.38d012cba8f79a3312ba6f05a27fb345.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/map-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
