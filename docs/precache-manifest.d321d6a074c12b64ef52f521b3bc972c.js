bd32a0d3cec18a48048404fda530137697447de2
Rafal2228 <rafal2228@gmail.com>
2019-01-15 13:40:53 +0100

    chore(docs): add docs

---
+++ docs/precache-manifest.d321d6a074c12b64ef52f521b3bc972c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3627da37546152aa8350",
+    "url": "/static/js/main.3627da37.chunk.js"
+  },
+  {
+    "revision": "96a8197ec1a30fb9b6c1",
+    "url": "/static/js/1.96a8197e.chunk.js"
+  },
+  {
+    "revision": "3627da37546152aa8350",
+    "url": "/static/css/main.cce610b2.chunk.css"
+  },
+  {
+    "revision": "0d67535bbf250979e7d8d0d1c34d8720",
+    "url": "/index.html"
+  }
+];
